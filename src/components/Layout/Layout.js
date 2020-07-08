d0f2d5b1600aafe80a84978a89774176eac41254
curtisbellamy <35937828+curtisbellamy@users.noreply.github.com>
2020-07-07 22:51:08 -0700

    Added modal and changed Aux naming conflicts

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 
 const layout = (props) => (
-    <Aux>
+    <Auxiliary>
         <div>Toolbar, Sidebar, Backdrop</div>
         <main className={classes.Content}>{props.children}</main>
-    </Aux>
+    </Auxiliary>
 );
 
 export default layout;
